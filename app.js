bcf991f76b42edb120aa8fa321ac60811d508a8f
Im-Hassan-wd <alhassansahih@gmail.com>
2021-08-24 11:08:52 +0100

    creating awesome beat

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 addEventListener('load', () => {
     const beats = document.querySelectorAll('.pads audio');
-    console.log(beats);
+    const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector('.visual');
+
+    const colors = ['#60d394', '#d36060', '#c060d3', '#d3d160', '#6860d3', '#60b2d3']
+
+    // let's get going with the sound
+    pads.forEach((pad, i) => {
+        pad.addEventListener('click', function(e) {
+            beats[i].currentTime = 0;
+            beats[i].play();
+            createBeats(i,e);
+        });
+
+    });
+
+    // create beats
+    const createBeats = (i, e) => {
+        // const bubble  = document.createElement('div');
+        // visual.appendChild(bubble);
+        // bubble.style.backgroundColor = colors[i];
+        // bubble.style.animation = 'jump 1s ease';
+        // bubble.addEventListener('animationend', function() {
+        //     visual.removeChild(this);
+        // })
+        const bubble = e.target.children[1].cloneNode();
+        visual.appendChild(bubble);
+        bubble.style.animation = 'jump 1s ease';
+        bubble.addEventListener('animationend', function() {
+            visual.removeChild(this);
+        });
+    }
 });
